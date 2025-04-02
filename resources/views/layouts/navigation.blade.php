75ee0ac1c39613f453ed78dbedb929268974601d
asegumpan <ariel.segumpan@csav.edu.ph>
2025-04-02 17:40:52 +0800

    update nav menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -46,23 +46,6 @@
                             </x-slot>
                         </x-dropdown>
                     </div>
-
-
-
-
-                    {{-- <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.*')">
-                        {{ __('Users') }}
-                    </x-nav-link>
-
-
-                    <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.*')">
-                        {{ __('Roles') }}
-                    </x-nav-link>
-
-                    <x-nav-link :href="route('permissions.index')" :active="request()->routeIs('permissions.*')">
-                        {{ __('Permissions') }}
-                    </x-nav-link> --}}
-
                 </div>
             </div>
 
